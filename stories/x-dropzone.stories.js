8977b2b9c9132c9e5fa83de3bb20e78b8516b0e2
Willson Smith <willson.smith@shopify.com>
2021-03-25 16:55:57 -0400

    idk some rando story things becuase why not

--- stories/x-dropzone.stories.js
+++ stories/x-dropzone.stories.js
@@ -6,24 +6,34 @@ export default {
   component: 'x-dropzone'
 }
 
-const Template = (args) => html`
+const Template = (args) => {
+  return html`
   <style>
     .DropArea {
-      border: 1px solid black;
+      border: 2px dashed black;
+      border-radius: 6px;
 
       height: 200px;
       width: 500px;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      font-family: sans-serif;
     }
 
     x-dropzone[drag-over] .DropArea {
-      border: 1px dashed black;
+      border: 2px dashed rgba(155,155,155,1);
     }
   </style>
   <x-dropzone>
     <div class="DropArea">
-      <button>Hello</button>
+      <h2>Drag and drop</h2>
     </div>
   </x-dropzone>
+  </div>
 `
+  }
 
 export const Basic = Template.bind({})
