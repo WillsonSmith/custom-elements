22f754be1bc54d9a4638701484f8b340b6aea526
Willson Smith <me@willsonsmith.com>
2023-08-21 17:33:07 -0400

    Add basics for storybook

---
+++ web/storybook/stories/MyButton.stories.js
@@ -0,0 +1,22 @@
+export default {
+  title: 'Example/Button',
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Button = ({ backgroundColor = 'white', text }) => `
+    <button type="button" style="background-color: ${backgroundColor}">
+      ${text}
+    </button>
+  `;
+
+export const ButtonA = Button.bind({});
+ButtonA.args = {
+  text: 'Button A',
+};
+
+export const ButtonB = Button.bind({});
+ButtonB.args = {
+  text: 'Button B',
+};
